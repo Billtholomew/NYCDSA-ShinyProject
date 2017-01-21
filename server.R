c4ee869b95bece7162a54c03f368f9c0e3248cbb
= <billtholomew@gmail.com>
2017-01-21 17:33:10 -0500

    Updated code, now shows tornadoes, also has different Fs as different alphas

--- server.R
+++ server.R
@@ -1,14 +1,12 @@
-library(shiny)
 library(mapproj)
 library(dplyr)
 
 tornadoes = read.csv("tornadoes.csv")
-map("state", fill = FALSE, projection = "polyconic")
 
 function(input, output, session) {
   
-  # Filter the movies, returning a data frame
-  movies <- reactive({
+  # Filter the tornadoes, returning a data frame
+  selectedData <- reactive({
     # Due to dplyr issue #318, we need temp variables for input values
     minyear <- input$year[1]
     maxyear <- input$year[2]
@@ -21,11 +19,16 @@ function(input, output, session) {
         yr <= maxyear
       )
     
-    severity <- paste0("%", input$severity, "%")
-    t <- t %>% filter(fujita.estimate %like% severity)
+    t <- tornadoes %>% filter(fujita.estimate %in% severity)
     
     t <- as.data.frame(t)
-    
   })
+
+  output$plot1 <- renderPlot({
+    map("state", fill = FALSE, projection = "polyconic")
+    t = selectedData()
+    points(mapproject(list(x=t$slon, y=t$slat)), cex=0.33, col=alpha("blue", as.numeric(t$fujita.estimate) / 7 + (1/7)))
+  })
+  
   
 }
--- ui.R
+++ ui.R
@@ -1,23 +1,13 @@
 # ui.R
 
-# For dropdown menu
-actionLink <- function(inputId, ...) {
-  tags$a(href='javascript:void',
-         id=inputId,
-         class='action-button',
-         ...)
-}
-
-fluidPage(
-  titlePanel("Tornado Locations by Year and Severity"),
-  fluidRow(
-    column(3,
-           wellPanel(
-             sliderInput("year", "Year Occured", 1940, 2014, value = c(1970, 2014)),
-             checkboxGroupInput("fujita", "Severity*", sort(unique(tornadoes$fujita.estimate)), selected = unique(tornadoes$fujita.estimate)),
-             tags$small("*Fujita Scale values for severity are estimated based on property damage and distance traveled")
-           )
-    ),
-    column(9)
+pageWithSidebar(
+  headerPanel("Tornado Locations by Year and Severity"),
+  sidebarPanel(
+    sliderInput("year", "Year Occured", 1950, 2015, value = c(1970, 2014)),
+    checkboxGroupInput("severity", "Severity*", sort(unique(tornadoes$fujita.estimate)), selected = unique(tornadoes$fujita.estimate)),
+    tags$small("*Fujita Scale values for severity are estimated based on property damage and distance traveled")
+  ),
+  mainPanel(
+    plotOutput('plot1')
   )
 )
